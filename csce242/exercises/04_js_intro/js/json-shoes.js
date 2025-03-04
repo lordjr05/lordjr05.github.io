eb6b83b3ee091c5de1fd0fac287c42372111a333
lordjr05 <lordjr@email.sc.edu>
2025-03-04 09:00:26 -0500

    json class update

---
+++ csce242/exercises/04_js_intro/js/json-shoes.js
@@ -0,0 +1,20 @@
+const getShoes = async() => {
+    const url = "https://portiaportia.github.io/json/shoes.json";
+
+    try {
+        const response = await fetch(url);
+        return response.json();
+    } catch(error){
+        console.log(error);
+    }
+};
+const showShoes = async() => {
+    const shoes = await getShoes();
+    const shoesSection = document.getElementById("shoes-section");
+
+    shoes.forEach((shoe)=>{
+        shoesSection.append(shoe.name);
+    });
+};
+
+showShoes();
